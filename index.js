5cc14719ca957c0a203dec956dc76b65007ce028
SivakumarK2025 <sivakumark2015@outlook.com>
2025-04-11 23:15:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,583 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"index_atlas_1", frames: [[438,289,259,119],[699,289,259,119],[438,410,259,119],[699,410,259,119],[438,531,259,119],[0,0,825,287],[0,289,436,273]]}
+];
+
+
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.ignorePause = false;
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
+// symbols:
+
+
+
+(lib.CachedBmp_21 = function() {
+	this.initialize(ss["index_atlas_1"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_19 = function() {
+	this.initialize(ss["index_atlas_1"]);
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_17 = function() {
+	this.initialize(ss["index_atlas_1"]);
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_15 = function() {
+	this.initialize(ss["index_atlas_1"]);
+	this.gotoAndStop(3);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_13 = function() {
+	this.initialize(ss["index_atlas_1"]);
+	this.gotoAndStop(4);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_20 = function() {
+	this.initialize(ss["index_atlas_1"]);
+	this.gotoAndStop(5);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._1 = function() {
+	this.initialize(img._1);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,1443,2569);
+
+
+(lib._2 = function() {
+	this.initialize(img._2);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,1440,2560);
+
+
+(lib._3 = function() {
+	this.initialize(img._3);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,1440,2560);
+
+
+(lib._4 = function() {
+	this.initialize(img._4);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,1446,2563);
+
+
+(lib._5 = function() {
+	this.initialize(img._5);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,1440,2560);
+
+
+(lib._6 = function() {
+	this.initialize(img._6);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,1440,2560);
+
+
+(lib.Bitmap12 = function() {
+	this.initialize(ss["index_atlas_1"]);
+	this.gotoAndStop(6);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Part2 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.CachedBmp_21();
+	this.instance.setTransform(135.3,38.95,0.5,0.5);
+
+	this.instance_1 = new lib.CachedBmp_20();
+	this.instance_1.setTransform(0,0,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.instance}]}).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,412.5,143.5);
+
+
+(lib.Part1 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.CachedBmp_19();
+	this.instance.setTransform(135.3,38.95,0.5,0.5);
+
+	this.instance_1 = new lib.CachedBmp_20();
+	this.instance_1.setTransform(0,0,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.instance}]}).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,412.5,143.5);
+
+
+(lib.Next = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.CachedBmp_17();
+	this.instance.setTransform(135.3,38.95,0.5,0.5);
+
+	this.instance_1 = new lib.CachedBmp_20();
+	this.instance_1.setTransform(0,0,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.instance}]}).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,412.5,143.5);
+
+
+(lib.Main = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.CachedBmp_15();
+	this.instance.setTransform(135.3,38.95,0.5,0.5);
+
+	this.instance_1 = new lib.CachedBmp_20();
+	this.instance_1.setTransform(0,0,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.instance}]}).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,412.5,143.5);
+
+
+(lib.Back = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.CachedBmp_13();
+	this.instance.setTransform(135.3,38.95,0.5,0.5);
+
+	this.instance_1 = new lib.CachedBmp_20();
+	this.instance_1.setTransform(0,0,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.instance}]}).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,412.5,143.5);
+
+
+// stage content:
+(lib.GardenGuide = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	this.actionFrames = [0];
+	// timeline functions:
+	this.frame_0 = function() {
+		/* Stop at This Frame
+		The  timeline will stop/pause at the frame where you insert this code.
+		Can also be used to stop/pause the timeline of movieclips.
+		*/
+		
+		var frameNumber = 0;
+		
+		var firstframe=0, lastframe=0;
+		
+		this.stop();
+		
+		/* Click to Go to Frame and Stop
+		Clicking on the specified symbol instance moves the playhead to the specified frame in the timeline and stops the movie.
+		Can be used on the main timeline or on movie clip timelines.
+		
+		Instructions:
+		1. Replace the number 5 in the code below with the frame number you would like the playhead to move to when the symbol instance is clicked.
+		2.Frame numbers in EaselJS start at 0 instead of 1
+		*/
+		
+		
+		this.Main.addEventListener("click", fl_ClickToGoToAndStopAtFrame.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame()
+		{
+			firstframe=0;
+			lastframe=0;
+			frameNumber = 0;
+			this.gotoAndStop(0);
+			console.log("Current frame number:", frameNumber);
+		}
+		
+		
+		/* Click to Go to Frame and Stop
+		Clicking on the specified symbol instance moves the playhead to the specified frame in the timeline and stops the movie.
+		Can be used on the main timeline or on movie clip timelines.
+		
+		Instructions:
+		1. Replace the number 5 in the code below with the frame number you would like the playhead to move to when the symbol instance is clicked.
+		2.Frame numbers in EaselJS start at 0 instead of 1
+		*/
+		
+		
+		this.Part1.addEventListener("click", fl_ClickToGoToAndStopAtFrame_2.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_2()
+		{
+			firstframe=1;
+			lastframe=3;
+			frameNumber = 1;
+			this.gotoAndStop(1);
+			console.log("Current frame number:", frameNumber);
+		}
+		
+		
+		/* Click to Go to Frame and Stop
+		Clicking on the specified symbol instance moves the playhead to the specified frame in the timeline and stops the movie.
+		Can be used on the main timeline or on movie clip timelines.
+		
+		Instructions:
+		1. Replace the number 5 in the code below with the frame number you would like the playhead to move to when the symbol instance is clicked.
+		2.Frame numbers in EaselJS start at 0 instead of 1
+		*/
+		
+		
+		this.Part2.addEventListener("click", fl_ClickToGoToAndStopAtFrame_3.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_3()
+		{
+			firstframe=4;
+			lastframe=6;
+			frameNumber = 4;
+			this.gotoAndStop(4);
+			console.log("Current frame number:", frameNumber);
+		}
+		
+		
+		
+		
+		/* Click to Go to Frame and Stop
+		Clicking on the specified symbol instance moves the playhead to the specified frame in the timeline and stops the movie.
+		Can be used on the main timeline or on movie clip timelines.
+		
+		Instructions:
+		1. Replace the number 5 in the code below with the frame number you would like the playhead to move to when the symbol instance is clicked.
+		2.Frame numbers in EaselJS start at 0 instead of 1
+		*/
+		
+		
+		this.Next.addEventListener("click", fl_ClickToGoToAndStopAtFrame_4.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_4()
+		{
+			if(frameNumber < lastframe)
+			{
+				frameNumber++;
+				this.gotoAndStop(frameNumber);
+			}
+			console.log("Current frame number:", frameNumber);
+		}
+		
+		
+		
+		
+		/* Click to Go to Frame and Stop
+		Clicking on the specified symbol instance moves the playhead to the specified frame in the timeline and stops the movie.
+		Can be used on the main timeline or on movie clip timelines.
+		
+		Instructions:
+		1. Replace the number 5 in the code below with the frame number you would like the playhead to move to when the symbol instance is clicked.
+		2.Frame numbers in EaselJS start at 0 instead of 1
+		*/
+		
+		
+		this.Back.addEventListener("click", fl_ClickToGoToAndStopAtFrame_5.bind(this));
+		
+		function fl_ClickToGoToAndStopAtFrame_5()
+		{
+			if(frameNumber > firstframe)
+			{
+				frameNumber--;
+				this.gotoAndStop(frameNumber);
+			}
+			console.log("Current frame number:", frameNumber);
+		}
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(7));
+
+	// Pages
+	this.instance = new lib._1();
+	this.instance.setTransform(91,-227);
+
+	this.instance_1 = new lib._2();
+	this.instance_1.setTransform(91,-204);
+
+	this.instance_2 = new lib._3();
+	this.instance_2.setTransform(91,-187);
+
+	this.instance_3 = new lib._4();
+	this.instance_3.setTransform(91,-218);
+
+	this.instance_4 = new lib._5();
+	this.instance_4.setTransform(91,-182);
+
+	this.instance_5 = new lib._6();
+	this.instance_5.setTransform(91,-225);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[]}).to({state:[{t:this.instance}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_2}]},1).to({state:[{t:this.instance_3}]},1).to({state:[{t:this.instance_4}]},1).to({state:[{t:this.instance_5}]},1).wait(1));
+
+	// BG
+	this.instance_6 = new lib.Bitmap12();
+	this.instance_6.setTransform(57,1331,3.4404,3.4405);
+
+	this.Next = new lib.Next();
+	this.Next.name = "Next";
+	this.Next.setTransform(1297.25,2474.5,1,1,0,0,0,206.3,71.8);
+	this.Next.visible = false;
+	new cjs.ButtonHelper(this.Next, 0, 1, 1);
+
+	this.Main = new lib.Main();
+	this.Main.name = "Main";
+	this.Main.setTransform(808.5,2474.5,1,1,0,0,0,206.3,71.8);
+	new cjs.ButtonHelper(this.Main, 0, 1, 1);
+
+	this.Back = new lib.Back();
+	this.Back.name = "Back";
+	this.Back.setTransform(297.7,2474.5,1,1,0,0,0,206.3,71.8);
+	this.Back.visible = false;
+	new cjs.ButtonHelper(this.Back, 0, 1, 1);
+
+	this.Part2 = new lib.Part2();
+	this.Part2.name = "Part2";
+	this.Part2.setTransform(456.4,897.65,1,1,0,0,0,206.3,71.8);
+	new cjs.ButtonHelper(this.Part2, 0, 1, 1);
+
+	this.Part1 = new lib.Part1();
+	this.Part1.name = "Part1";
+	this.Part1.setTransform(456.4,580.85,1,1,0,0,0,206.3,71.8);
+	new cjs.ButtonHelper(this.Part1, 0, 1, 1);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.Part1,p:{x:456.4,visible:true}},{t:this.Part2,p:{x:456.4,visible:true}},{t:this.Back,p:{visible:false}},{t:this.Main},{t:this.Next,p:{visible:false}},{t:this.instance_6}]}).to({state:[{t:this.Part1,p:{x:445.5,visible:false}},{t:this.Part2,p:{x:467.3,visible:false}},{t:this.Back,p:{visible:false}},{t:this.Main},{t:this.Next,p:{visible:true}}]},1).to({state:[{t:this.Part1,p:{x:445.5,visible:false}},{t:this.Part2,p:{x:467.3,visible:false}},{t:this.Back,p:{visible:true}},{t:this.Main},{t:this.Next,p:{visible:true}}]},1).to({state:[{t:this.Part1,p:{x:445.5,visible:false}},{t:this.Part2,p:{x:467.3,visible:false}},{t:this.Back,p:{visible:true}},{t:this.Main},{t:this.Next,p:{visible:false}}]},1).to({state:[{t:this.Part1,p:{x:445.5,visible:false}},{t:this.Part2,p:{x:467.3,visible:false}},{t:this.Back,p:{visible:false}},{t:this.Main},{t:this.Next,p:{visible:true}}]},1).to({state:[{t:this.Part1,p:{x:445.5,visible:false}},{t:this.Part2,p:{x:467.3,visible:false}},{t:this.Back,p:{visible:true}},{t:this.Main},{t:this.Next,p:{visible:true}}]},1).to({state:[{t:this.Part1,p:{x:445.5,visible:false}},{t:this.Part2,p:{x:467.3,visible:false}},{t:this.Back,p:{visible:true}},{t:this.Main},{t:this.Next,p:{visible:false}}]},1).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(857,1053,700,1493.1999999999998);
+// library properties:
+lib.properties = {
+	id: '561811462F5F6147A5DE3641E0B8B2EA',
+	width: 1600,
+	height: 2560,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/_1.jpg", id:"_1"},
+		{src:"images/_2.jpg", id:"_2"},
+		{src:"images/_3.jpg", id:"_3"},
+		{src:"images/_4.jpg", id:"_4"},
+		{src:"images/_5.jpg", id:"_5"},
+		{src:"images/_6.jpg", id:"_6"},
+		{src:"images/index_atlas_1.png", id:"index_atlas_1"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['561811462F5F6147A5DE3641E0B8B2EA'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused || stageChild.ignorePause){
+			stageChild.syncStreamSounds();
+		}
+	}
+}
+an.handleFilterCache = function(event) {
+	if(!event.paused){
+		var target = event.target;
+		if(target){
+			if(target.filterCacheList){
+				for(var index = 0; index < target.filterCacheList.length ; index++){
+					var cacheInst = target.filterCacheList[index];
+					if((cacheInst.startFrame <= target.currentFrame) && (target.currentFrame <= cacheInst.endFrame)){
+						cacheInst.instance.cache(cacheInst.x, cacheInst.y, cacheInst.w, cacheInst.h);
+					}
+				}
+			}
+		}
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
